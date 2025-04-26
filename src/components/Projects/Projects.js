3616d6d707da90541df152ecc63a8594c4508ae8
Kristyle Marie Modin <kmgmodin@addu.edu.ph>
2025-04-26 14:46:23 +0800

    Minor Changes - add a project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
 import orv from "../../Assets/Projects/orv.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import EmployeeSystem from "../../Assets/Projects/employeesystem.png";
 import KapitanSisig from "../../Assets/Projects/kapitansisig.png";
 
 function Projects() {
   return (
     <Container fluid className="project-section">
-      <Particle />
       <Container>
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
@@ -26,7 +21,7 @@ function Projects() {
               imgPath={orv}
               isBlog={false}
               title="Omniscient Reader's Viewpoint"
-              description="A digital project inspired by the Korean web novel 'Omniscient Reader’s Viewpoint.' This presentation explores its compelling narrative, key characters, and major themes, offering an engaging look into a world where fiction becomes reality."              
+              description="A digital project inspired by the Korean web novel Omniscient Reader’s Viewpoint, built using HTML, CSS, and JavaScript. This presentation explores its compelling narrative, key characters, and major themes, offering an engaging look into a world where fiction becomes reality."              
               demoLink="https://kristylemodin.github.io/orvKhimmy_AppDev/"
             />
           </Col>
@@ -36,9 +31,20 @@ function Projects() {
               imgPath={KapitanSisig}
               isBlog={false}
               title="Kapitan Sisig POS System"
-              description="A custom-built Point of Sale and Inventory Management System designed for Kapitan Sisig, a small food business. This system streamlines daily operations by automating sales transactions, inventory tracking, and reporting. It enhances operational efficiency, minimizes manual errors, and supports business scalability by accommodating growing order volumes and potential multi-location expansion."
+              description="A custom Point of Sale and Inventory System for Kapitan Sisig, built with PHP, HTML, and JavaScript. It automates sales transactions, inventory tracking, and reporting, improving efficiency, reducing errors, and supporting business growth and multi-location expansion."
               ghLink="https://github.com/KristyleModin/KapitanSisig_POS_System"
-              demoLink="https://drive.google.com/file/d/1XemUd8KnZWcQtzZ6sxRRh7GbWiZNCIx_/view?usp=sharing"
+              demoLink="https://drive.google.com/file/d/11aVU0rQ0rbXuelt7WBrZ6q08v7OoRB7J/view?usp=sharing"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={EmployeeSystem}
+              isBlog={false}
+              title="Employee Management System"
+              description="A simple Employee Management System built with Python, HTML, and CSS. It supports creating, reading, updating, and deleting (CRUD) employee records through a web-based interface."
+              ghLink="https://github.com/KristyleModin/Employee-Management-System"
+              demoLink="https://drive.google.com/file/d/1uq4Z4CeRwnRu7ZNf_i-ffbh9ztt8zWVJ/view?usp=sharing"
             />
           </Col>
         </Row>
